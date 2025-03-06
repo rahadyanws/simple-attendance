41dc9a816b261698455ea1ee96f93d0bb87a2939
Rahadyan <rahadyanws@gmail.com>
2025-03-06 22:33:30 +0700

    refactor(auth): update response payload

--- __test__/authController.test.js
+++ __test__/authController.test.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 
 jest.mock('../src/configs/mysql', () => {
     const mockQuery = jest.fn(); // Create a mock for query
-  
     return {
       pool: {
         promise: jest.fn(() => ({
@@ -20,63 +19,117 @@ jest.mock('jsonwebtoken');
 jest.mock('bcrypt');
 
 describe('login', () => {
-  let req, res;
+  let mockReq, mockRes, mockPoolQuery;
 
   beforeEach(() => {
-    req = {
-      body: { email: 'test@example.com', password: 'password123' },
+    mockReq = {
+      body: {
+        email: 'test@example.com',
+        password: 'password123',
+      },
     };
-    res = {
+    mockRes = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
-    jwt.sign.mockClear();
     pool.promise().query.mockClear();
     bcrypt.compare.mockClear();
+    jwt.sign.mockClear();
   });
 
-  it('should login user successfully', async () => {
+  it('should return 404 if email is not found', async () => {
+    pool.promise().query.mockResolvedValue([[]]); // No user found
+
+    await login(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(404);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      status: 'error',
+      code: 404,
+      message: 'Email is not found!',
+    });
+  });
+
+  it('should return 400 if password is wrong', async () => {
+    pool.promise().query.mockResolvedValue([[{ user_id: '123', name: 'Test User', email: 'test@example.com', password: 'hashedPassword' }]]);
+    bcrypt.compare.mockResolvedValue(false);
+
+    await login(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(400);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      status: 'error',
+      code: 400,
+      message: 'Wrong password!',
+    });
+  });
+
+  it('should return 200 with accessToken and user data on successful login', async () => {
     pool.promise().query.mockResolvedValue([[{ user_id: '123', name: 'Test User', email: 'test@example.com', password: 'hashedPassword' }]]);
     bcrypt.compare.mockResolvedValue(true);
     jwt.sign.mockReturnValue('mockAccessToken');
 
-    await login(req, res);
+    await login(mockReq, mockRes);
 
-    expect(pool.promise().query).toHaveBeenCalledWith('SELECT * FROM users WHERE email = ?', ['test@example.com']);
-    expect(bcrypt.compare).toHaveBeenCalledWith('password123', 'hashedPassword');
-    expect(jwt.sign).toHaveBeenCalledWith(
-      { userId: '123', name: 'Test User', email: 'test@example.com' },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '1d' }
-    );
-    expect(res.json).toHaveBeenCalledWith({ accessToken: 'mockAccessToken' });
+    expect(mockRes.json).toHaveBeenCalledWith({
+      status: 'success',
+      code: 200,
+      message: 'Login successful',
+      data: {
+        accessToken: 'mockAccessToken',
+        user: {
+          userId: '123',
+          name: 'Test User',
+          email: 'test@example.com',
+        },
+      },
+    });
+
+    expect(jwt.sign).toHaveBeenCalled();
   });
 
-  it('should return 404 if email is not found', async () => {
-    pool.promise().query.mockResolvedValue([[]]);
-
-    await login(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(404);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Email is not found!' });
-  });
-
-  it('should return 400 if password is wrong', async () => {
-    pool.promise().query.mockResolvedValue([[{ userId: '123', name: 'Test User', email: 'test@example.com', password: 'hashedPassword' }]]);
-    bcrypt.compare.mockResolvedValue(false);
-
-    await login(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Wrong password!' });
-  });
-
-  it('should handle errors', async () => {
+  it('should return 500 if an error occurs', async () => {
     pool.promise().query.mockRejectedValue(new Error('Database error'));
 
-    await login(req, res);
+    await login(mockReq, mockRes);
 
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Database error' });
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      status: 'error',
+      code: 500,
+      message: 'Internal Server Error',
+      error: 'Database error',
+    });
+  });
+
+  it('should handle bcrypt error', async () => {
+    pool.promise().query.mockResolvedValue([[{ user_id: '123', name: 'Test User', email: 'test@example.com', password: 'hashedPassword' }]]);
+    bcrypt.compare.mockRejectedValue(new Error("bcrypt error"));
+
+    await login(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      status: 'error',
+      code: 500,
+      message: 'Internal Server Error',
+      error: 'bcrypt error',
+    });
+  });
+
+  it('should handle JWT error', async () => {
+    pool.promise().query.mockResolvedValue([[{ user_id: '123', name: 'Test User', email: 'test@example.com', password: 'hashedPassword' }]]);
+    bcrypt.compare.mockResolvedValue(true);
+    jwt.sign.mockImplementation(() => { throw new Error('JWT Error'); });
+
+    await login(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      status: 'error',
+      code: 500,
+      message: 'Internal Server Error',
+      error: 'JWT Error',
+    });
   });
 });
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,27 +6,54 @@ const bcrypt = require('bcrypt');
 const login = async (req, res) => {
   try {
     const [user] = await pool.promise().query('SELECT * FROM users WHERE email = ?', [req.body.email]);
+
     if (user.length === 0) {
-      return res.status(404).json({ message: 'Email is not found!' });
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'Email is not found!',
+      });
     }
+
     const match = await bcrypt.compare(req.body.password, user[0].password);
+
     if (!match) {
-      return res.status(400).json({ message: 'Wrong password!' });
+      return res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: 'Wrong password!',
+      });
     }
-    const userId = user[0].user_id;
-    const name = user[0].name;
-    const email = user[0].email;
-    
+
+    const userData = {
+      userId: user[0].user_id,
+      name: user[0].name,
+      email: user[0].email,
+    };
+
     const accessToken = jwt.sign(
-      { userId, name, email },
+      userData,
       process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: '1d',
-      }
+      { expiresIn: '1d' }
     );
-    res.json({ accessToken });
+
+    res.json({
+      status: 'success',
+      code: 200,
+      message: 'Login successful',
+      data: {
+        accessToken: accessToken,
+        user: userData,
+      },
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    console.error('Login error:', error);
+    res.status(500).json({
+      status: 'error',
+      code: 500,
+      message: 'Internal Server Error',
+      error: error.message,
+    });
   }
 };
 
